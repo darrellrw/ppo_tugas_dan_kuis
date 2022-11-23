3c9515cf1f7914ecf84843e5e120323c3ad25b63
darrellrw <11211028@student.itk.ac.id>
2022-11-23 18:48:44 +0800

    Update 23/11/22 18:49

--- Tugas 3/simplex.py
+++ Tugas 3/simplex.py
@@ -1,11 +1,14 @@
 from tabulate import tabulate
 
+#Fungsi Z
 def z(x1, x2):
     return 8 * x1 + 6 * x2
 
+#Fungsi Batas 1
 def fb1(x1, x2):
     return 4 * x1 + 2 * x2 <= 60
 
+#Fungsi Batas 2
 def fb2(x1, x2):
     return 2 * x1 + 4 * x2 >= 48
 
@@ -27,9 +30,14 @@ class Simplex:
         self.headers.append("rhs")
         self.headers.append("ratio")
 
+        self.ratio = 0
+        self.pivot = 0
+
+    #Menampilkan Tabel
     def showTable(self):
         print(tabulate(self.tab, self.headers, tablefmt="grid"))
 
+    #Menentukan nilai awal pada tabel (Kanonik)
     def initTableu(self):
         if(len(self.tab) == 0):
             tab0 = []
@@ -53,9 +61,7 @@ class Simplex:
                 self.tab.append(tabx)
                 self.tab[i + 1][len(self.z)+ 1 + i] = 1
 
-        self.columnKey()
-        self.rowKey()
-
+    #Mencari Kunci Kolom berdasarkan nilai yang paling negatif
     def columnKey(self):
         xTab = []
         mint = []
@@ -67,45 +73,64 @@ class Simplex:
             xTab.append(min(mint))
         for i in range(len(self.tab)):
             if(min(xTab) in self.tab[i][:3]):
-                columnK = self.tab[i].index(min(xTab))
+                columnK = self.tab[i].index(min(xTab)) #Operasi untuk mencari nilai paling negatif
         self.ck = columnK
 
+    #Mencari Kunci Baris berdasarkan ratio terkecil
     def rowKey(self):
         cTab = []
         for i in range(len(self.slack)):
-            ratio = self.tab[i + 1][-2] / self.tab[i + 1][self.ck]
+            ratio = self.tab[i + 1][-2] / self.tab[i + 1][self.ck] #Operasi untuk mencari ratio
             self.tab[i + 1][-1] = ratio
             cTab.append(ratio)
-        self.rk = cTab.index(min(cTab)) + 1
+        self.ratio = min(cTab)
+        self.rk = cTab.index(min(cTab)) + 1 #Operasi untuk mencari kunci baris
 
+    #Menghitung Operasi Baris Dasar
     def obd(self):
-        pivot = self.tab[self.ck][self.rk]
-        for i in range(len(self.tab[self.ck]) - 1):
-            self.tab[self.ck][i] = self.tab[self.ck][i] / pivot
+        pivot = self.tab[self.ck][self.rk] #Menentukan Pivot
+        self.pivot = pivot
+        for i in range(len(self.tab[self.ck]) - 1): #Perulangan untuk Kunci Baris
+            self.tab[self.ck][i] = self.tab[self.ck][i] / pivot #Nilai pada Baris Kunci dibagi dengan pivot
 
-        for j in range(len(self.tab) - 1):
-            oper = self.tab[self.ck - j - 1][self.rk]
-            for k in range(len(self.tab[self.ck]) - 1):
-                self.tab[self.ck - j - 1][k] = self.tab[self.ck - j - 1][k] - (oper * self.tab[self.ck][k])
-        
-        try:
-            self.columnKey()
-            self.rowKey()
-        except:
-            None
+        for j in range(len(self.tab) - 1): #Perulangan untuk baris lainnya
+            oper = self.tab[self.ck - j - 1][self.rk] #Operasi pembentuk nol
+            print(f"b{self.tab.index(self.tab[self.ck - j - 1])} - ({oper} * b{self.ck})")
+            for k in range(len(self.tab[self.ck]) - 1): #Perulangan untuk setiap nilai pada baris
+                self.tab[self.ck - j - 1][k] = self.tab[self.ck - j - 1][k] - (oper * self.tab[self.ck][k]) #Operasi Baris Dasar
 
+        for i in range(len(self.slack)): #Perulangan untuk membuat nol ratio
+            self.tab[i + 1][-1] = 0
+
+    #Fungsi untuk menjalankan Metode Simplex
     def run(self):
+        print("Tableau 1")
         self.initTableu()
         self.showTable()
-        print("")
-
-        self.obd()
+        self.columnKey()
+        self.rowKey()
+        print(f"\nKolom Kunci: Kolom-{self.ck + 1}")
+        print(f"Baris Kunci: Baris-{self.rk + 1}, Ratio Terkecil: {self.ratio}")
         self.showTable()
-        print("")
-
+        print("\nOperasi Baris Dasar")
         self.obd()
+        print(f"Pivot: {self.pivot}")
         self.showTable()
-        print("")
+
+        print("\nTableau 2")
+        self.showTable()
+        self.columnKey()
+        self.rowKey()
+        print(f"\nKolom Kunci: Kolom-{self.ck + 1}")
+        print(f"Baris Kunci: Baris-{self.rk + 1}, Ratio Terkecil: {self.ratio}")
+        self.showTable()
+        print("\nOperasi Baris Dasar")
+        self.obd()
+        print(f"Pivot: {self.pivot}")
+        self.showTable()
+
+        print("\nTableau 3")
+        self.showTable()
 
         print(f"Jadi, nilai x1 = {self.tab[1][-2]} dan x2 = {self.tab[2][-2]}.")
         print(f"Maka nilai maksimum z adalah {self.tab[0][-2]}")
